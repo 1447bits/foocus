c16561859c94bc8fd5ea1931a7ad8a18ca920438
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2023-04-23 16:18:38 +0530

    added login page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,41 +6,65 @@ import navInfo from "../icons/nav-info.png"
 import navGithub from "../icons/nav-github.png"
 import navBucket from "../icons/nav-bucket.png"
 
+import { motion } from "framer-motion";
+import { type } from "@testing-library/user-event/dist/type";
+
 export class Nav extends React.Component {
 
     render() {
         const navIconStyles = {
-            width : "50px",
-            height : "50px",
-            background : "#D6D8FA",
-            borderRadius : "50%",
-            display : "flex",
-            justifyContent : "center",
-            alignItems : "center",
-            cursor : "pointer"
+            width: "50px",
+            height: "50px",
+            // background: "#d7d8ec",
+            background: "#cccee5",
+            borderRadius: "25px",
+            fontWeight :"bold",
+            fontSize : "1.1em",
+            display: "flex",
+            justifyContent: "left",
+            overflow: "hidden",
+            gap: "1em",
+            alignItems: "center",
+            cursor: "pointer",
+            transition: "0.3s",
+            paddingInline : "10px",
         }
-        
-        return <div id="nav-bar" style={{padding: "0.5em", display: "flex"}}>
-            <span id="nav-logo" style={{display : "flex", gap : "0.5em", alignItems:"center"}}>
-                <img src={navIcon} style={{width : "2em"}} />
-                <h1 style={{ fontSize : "1.7em", color : "#202227", textShadow : "-4px -2px 1px rgba(32, 34, 39, 0.49)" }}>
+        const OnHoverStyles = {
+            width: 'fit-content'
+        }
+
+        return <div id="nav-bar" style={{ padding: "0.5em", display: "flex" }}>
+            <div id="nav-logo" style={{ display: "flex", gap: "0.5em", alignItems: "center" }}>
+                <img src={navIcon} style={{ width: "2em" }} alt="icon" />
+                <h1 style={{
+                    fontSize: "1.7em",
+                    color: "#202227",
+                    textShadow: "-4px -2px 1px rgba(32, 34, 39, 0.49)"
+                }}>
                     FOOCUS
                 </h1>
-            </span>
-            <span id="nav-icons" style={{ display: "flex", marginInline: "auto", gap: "1em" }}>
-                <span className="nav-icon" style={navIconStyles} >
-                    <img src={navHome} />
-                </span>
-                <span className="nav-icon" style={navIconStyles} >
-                    <img src={navBucket} />
-                </span>
-                <span className="nav-icon" style={navIconStyles} >
-                    <img src={navInfo} />
-                </span>
-                <span className="nav-icon" style={navIconStyles} >
-                    <img src={navGithub} />
-                </span>
-            </span>
+            </div>
+            <div id="nav-icons" style={{display : "flex", marginLeft: "calc((50vw - (200px + (1em * 5)))", gap: "1em" }}>
+                <motion.div className="nav-icon" style={navIconStyles} 
+                // whileHover={OnHoverStyles}
+                // transition={{type: "just"}}
+                >
+                    <img src={navHome} alt="icon" />
+                    <p>Home</p>
+                </motion.div>
+                <motion.div className="nav-icon" style={navIconStyles} >
+                    <img src={navBucket} alt="icon" />
+                    <p>Home</p>
+                </motion.div>
+                <motion.div className="nav-icon" style={navIconStyles} >
+                    <img src={navInfo} alt="icon" />
+                    <p>Home</p>
+                </motion.div>
+                <motion.div className="nav-icon" style={navIconStyles} >
+                    <img src={navGithub} alt="icon" />
+                    <p>Home</p>
+                </motion.div>
+            </div>
         </div>
     }
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,27 +1,99 @@
 import React from "react";
 import ProfileIcon from "../icons/profile-icon.png"
+import googleSignin from "../images/googlesignin.png"
+import LoginPageBG from "../images/loginpageBG.png"
 
 export class LoginPage extends React.Component {
     render() {
+        const labelStyles = {
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.2em",
+            color: "#7D7E82",
+        }
+        const inputStyles = {
+            // width: "60%",
+            color: "#7D7F56",
+            background: "#E8E8F8",
+            fontSize: "1.5em",
+            padding: "0.3em",
+            borderRadius: "5px",
+            border: "1px solid #B3B3B3"
+        }
+
         return <div id="LoginPage"
             style={{ height: "95vh" }}
         >
-            <div id="loginbox"
-                style={{ width: "60%", 
-                marginInline: "auto", 
-                marginTop: "3em",
-                padding: "1em", 
-                background: "rgba(232, 232, 250, 0.5)", 
-                boxShadow: "0px 0px 20px #959595", 
-                backdropFilter: "blur(8px)", 
-                borderRadius: "0.5em" }}
+            <div id="loginPageBG" 
+            style={{
+                position: "absolute",
+                top: "30vh",
+                left :"5vw",
+            }}
             >
-                <span style={{ display : "flex" , gap: "1em", marginInline: "auto", width: "fit-content"}}>
-                    <img src={ProfileIcon} style={{ width: "2.2em" }} />
+                <img src={LoginPageBG} width={"100%"} alt="background"/>
+            </div>
+            <div id="loginbox"
+                style={{
+                    width: "60%",
+                    maxWidth: "650px",
+                    marginInline: "auto",
+                    marginTop: "15vh",
+                    padding: "3%",
+                    background: "rgba(232, 232, 250, 0.5)",
+                    boxShadow: "0px 0px 20px #959595",
+                    backdropFilter: "blur(8px)",
+                    borderRadius: "0.5em"
+                }}
+            >
+                <span style={{ display: "flex", gap: "1em", marginInline: "auto", width: "fit-content" }}>
+                    <img src={ProfileIcon} style={{ width: "2.2em" }} alt="icon" />
                     <h1 style={{ color: "#403DEE" }}>LOGIN</h1>
                 </span>
+                <div id="loginInputs" style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "60%",
+                    marginInline: "auto",
+                    marginTop: "2em",
+                    gap: "1em"
+                }}>
+                    <label style={labelStyles}>
+                        Email
+                        <input style={inputStyles} type="text" />
+                    </label>
+                    <label style={labelStyles}>
+                        Password
+                        <input style={inputStyles} type="password" />
+                    </label>
+                    <button id="LoginBtn" style={{
+                        all: "unset",
+                        background: "#6970EF",
+                        textAlign: "center",
+                        padding: "0.4em",
+                        borderRadius: "6px",
+                        marginInline: "auto",
+                        color: "#E3E3F7",
+                        fontWeight: "bold",
+                        width: "40%",
+                        cursor :"pointer",
+                        fontSize: "1.3em",
+                        marginTop: "1em",                        
+                    }}>
+                        LOGIN
+                    </button>
+                    <span style={{ textAlign: "center" }}>
+                        ( OR )
+                    </span>
+                    <span>
+                        <img src={googleSignin}
+                            width={"100%"}
+                            alt="google-signin"
+                        />
+                    </span>
+                </div>
 
-            </div>    
+            </div>
         </div>
     }
 }
