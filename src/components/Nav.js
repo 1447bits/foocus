0f5378e593d64a78fbef50777aa4408db827118c
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2023-04-24 22:18:47 +0530

    fixed loginpage designs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import "../styles/nav.css"
-import navIcon from "../icons/focus-icon.png"
+import Website_icon from "../icons/focus-icon.png"
 import navHome from "../icons/nav-home.png"
 import navInfo from "../icons/nav-info.png"
 import navGithub from "../icons/nav-github.png"
@@ -8,11 +7,10 @@ import navBucket from "../icons/nav-bucket.png"
 
 import { motion } from "framer-motion";
 
-
 export function Nav() {
 
-    const [page, setpage] = useState("home")
-    const [currpage, setcurrpage] = useState("home")
+    const [page, setpage] = useState("Home")
+    const [currpage, setcurrpage] = useState("Home")
 
     const navIconStyles = {
         height: "50px",
@@ -20,19 +18,18 @@ export function Nav() {
         background: "#cccee5",
         borderRadius: "25px",
         fontWeight: "bold",
-        fontSize: "1.1em",
+        fontSize: "1.3em",
         display: "flex",
-        justifyContent: "left",
         overflow: "hidden",
-        gap: "1em",
+        gap: "0.5em",
         alignItems: "center",
         cursor: "pointer",
         paddingInline: "10px",
     }
 
-    return <div id="nav-bar" style={{ padding: "0.5em", display: "flex" }}>
+    return <div id="nav-bar" style={{ padding: "0.5em", display: "flex", overflow : "hidden" }}>
         <div id="nav-logo" style={{ display: "flex", gap: "0.5em", alignItems: "center" }}>
-            <img src={navIcon} style={{ width: "2em" }} alt="icon" />
+            <img src={Website_icon} style={{ width: "2em" }} alt="icon" />
             <h1 style={{
                 fontSize: "1.7em",
                 color: "#202227",
@@ -44,27 +41,31 @@ export function Nav() {
         <div id="nav-icons" style={{ display: "flex", marginLeft: "calc((50vw - (200px + (1em * 5)))", gap: "1em" }}>
             <motion.div className="nav-icon"
                 onMouseEnter={() => {
-                    setpage("home")
+                    setpage("Home")
                 }}
                 onMouseLeave={() => {
                     setpage(currpage)
                 }}
                 animate={{
-                    width: page === "home" || currpage === "home" ? "fit-content" : "50px",
+                    width: page === "Home" || currpage === "Home" ? "fit-content" : "50px",
                 }}
                 style={navIconStyles}>
                 <img src={navHome} alt="icon" />
                 <p>Home</p>
             </motion.div>
+            {/* <NavLink
+                linkName="Home"
+                icon={navHome}
+            /> */}
             <motion.div className="nav-icon"
                 onMouseEnter={() => {
-                    setpage("buckets")
+                    setpage("Buckets")
                 }}
                 onMouseLeave={() => {
                     setpage(currpage)
                 }}
                 animate={{
-                    width: page === "buckets" || currpage === "buckets" ? "fit-content" : "50px",
+                    width: page === "Buckets" || currpage === "Buckets" ? "fit-content" : "50px",
                 }}
                 style={navIconStyles} >
                 <img src={navBucket} alt="icon" />
@@ -72,13 +73,13 @@ export function Nav() {
             </motion.div>
             <motion.div className="nav-icon"
                 onMouseEnter={() => {
-                    setpage("info")
+                    setpage("Info")
                 }}
                 onMouseLeave={() => {
                     setpage(currpage)
                 }}
                 animate={{
-                    width: page === "info" || currpage === "info" ? "fit-content" : "50px",
+                    width: page === "Info" || currpage === "Info" ? "fit-content" : "50px",
                 }}
                 style={navIconStyles} >
                 <img src={navInfo} alt="icon" />
@@ -86,13 +87,13 @@ export function Nav() {
             </motion.div>
             <motion.div className="nav-icon"
                 onMouseEnter={() => {
-                    setpage("contribute")
+                    setpage("Contribute")
                 }}
                 onMouseLeave={() => {
                     setpage(currpage)
                 }}
                 animate={{
-                    width: page === "contribute" || currpage === "contribute" ? "fit-content" : "50px",
+                    width: page === "Contribute" || currpage === "Contribute" ? "fit-content" : "50px",
                 }}
                 style={navIconStyles} >
                 <img src={navGithub} alt="icon" />
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -23,79 +23,89 @@ export class LoginPage extends React.Component {
         }
 
         return <div id="LoginPage"
-            style={{ height: "95vh" }}
-        >
-            <div id="loginPageBG" 
-            style={{
-                position: "absolute",
-                top: "45vh",
-                left :"5vw",
+            style={{ 
+                height: "100vh" ,
+                overflow : "auto",
             }}
-            >
-                <img src={LoginPageBG} width={"100%"} alt="background"/>
-            </div>
-            <div id="loginbox"
-                style={{
-                    width: "60%",
-                    maxWidth: "650px",
-                    marginInline: "auto",
-                    marginTop: "15vh",
-                    padding: "3%",
-                    background: "rgba(232, 232, 250, 0.5)",
-                    boxShadow: "0px 0px 20px #959595",
-                    backdropFilter: "blur(8px)",
-                    borderRadius: "0.5em"
-                }}
-            >
-                <span style={{ display: "flex", gap: "1em", marginInline: "auto", width: "fit-content" }}>
-                    <img src={ProfileIcon} style={{ width: "2.2em" }} alt="icon" />
-                    <h1 style={{ color: "#403DEE" }}>LOGIN</h1>
-                </span>
-                <div id="loginInputs" style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "60%",
-                    marginInline: "auto",
-                    marginTop: "2em",
-                    gap: "1em"
-                }}>
-                    <label style={labelStyles}>
-                        Email
-                        <input style={inputStyles} type="text" />
-                    </label>
-                    <label style={labelStyles}>
-                        Password
-                        <input style={inputStyles} type="password" />
-                    </label>
-                    <motion.button id="LoginBtn" style={{
-                        all: "unset",
-                        background: "#6970EF",
-                        textAlign: "center",
-                        padding: "0.4em",
-                        borderRadius: "6px",
-                        marginInline: "auto",
-                        color: "#E3E3F7",
-                        fontWeight: "bold",
-                        width: "40%",
-                        cursor :"pointer",
-                        fontSize: "1.3em",
-                        marginTop: "1em",                        
+        >
+            <div style={{
+                maxWidth : "750px",
+                marginInline : "auto",
+                overflow : "hidden",
+                // maxHeight : "600px"
+            }}>
+                <div id="loginPageBG"
+                    style={{
+                        position: "fixed",
+                        marginLeft : '-8%',
+                        marginTop : '15%',
                     }}
-                    whileHover={{opacity: "0.8"}}
-                    >
-                        LOGIN
-                    </motion.button>
-                    <span style={{ textAlign: "center" }}>
-                        ( OR )
-                    </span>
-                    <span>
-                        <img src={googleSignin}
-                            width={"100%"}
-                            alt="google-signin"
-                        />
-                    </span>
+                >
+                    <img src={LoginPageBG} width={"100%"} alt="background" />
                 </div>
+                <div id="loginbox"
+                    style={{
+                        width: "70%",
+                        maxWidth: "650px",
+                        marginInline: "auto",
+                        marginTop: "15vh",
+                        padding: "7%",
+                        background: "rgba(232, 232, 250, 0.5)",
+                        boxShadow: "0px 0px 20px #959595",
+                        backdropFilter: "blur(8px)",
+                        borderRadius: "0.5em"
+                    }}
+                >
+                    <span style={{ display: "flex", gap: "1em", marginInline: "auto", width: "fit-content" }}>
+                        <img src={ProfileIcon} style={{ width: "2.2em" }} alt="icon" />
+                        <h1 style={{ color: "#403DEE" }}>LOGIN</h1>
+                    </span>
+                    <div id="loginInputs" style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "70%",
+                        marginInline: "auto",
+                        marginTop: "2em",
+                        gap: "1em"
+                    }}>
+                        <label style={labelStyles}>
+                            Email
+                            <input style={inputStyles} type="email" />
+                        </label>
+                        <label style={labelStyles}>
+                            Password
+                            <input style={inputStyles} type="password" />
+                        </label>
+                        <motion.button id="LoginBtn" style={{
+                            all: "unset",
+                            background: "#6970EF",
+                            textAlign: "center",
+                            padding: "0.4em",
+                            borderRadius: "6px",
+                            marginInline: "auto",
+                            color: "#E3E3F7",
+                            fontWeight: "bold",
+                            width: "40%",
+                            cursor: "pointer",
+                            fontSize: "1.3em",
+                            marginTop: "1em",
+                        }}
+                            whileHover={{ opacity: "0.8" }}
+                        >
+                            LOGIN
+                        </motion.button>
+                        <span style={{ textAlign: "center" }}>
+                            ( OR )
+                        </span>
+                        <span>
+                            <img src={googleSignin}
+                                width={"100%"}
+                                alt="google-signin"
+                            />
+                        </span>
+                    </div>
 
+                </div>
             </div>
         </div>
     }
